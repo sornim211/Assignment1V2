30067c1e70776e01ef09e541cd5117e5729695a2
sornim211 <144562735+sornim211@users.noreply.github.com>
2025-07-20 13:55:33 -0400

    Adding the search button function

--- app/src/main/java/ca/georgiancollege/assignment1v2/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/assignment1v2/MainActivity.kt
@@ -1,11 +1,14 @@
 package ca.georgiancollege.assignment1v2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import ca.georgiancollege.assignment1v2.databinding.ActivityMainBinding
+import kotlin.toString
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding;
@@ -15,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(R.layout.activity_main)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        //Search button function
+        binding.buttonSearch.setOnClickListener {
+            val query = binding.editTextSearch.text.toString().trim()
+
+        }
 
     }
 }
